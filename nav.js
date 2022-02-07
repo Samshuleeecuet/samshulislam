d4845f180e6effd12aa2dad104b526d6210a289c
Samshul Islam <samshuleeeweb@gmail.com>
2022-02-07 18:25:11 +0600

    Home Update 7-2-22

--- nav.js
+++
@@ -1,7 +0,0 @@
-// typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["CEO of SAE","Engineer", "Developer", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
