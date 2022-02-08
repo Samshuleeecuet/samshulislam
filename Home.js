2026bde1c66c6827a9a87e7f087e72b9527d6fd9
Samshul Islam <samshuleeeweb@gmail.com>
2022-02-09 00:29:34 +0600

    9-2-22

--- Home.js
+++ Home.js
@@ -1,7 +1,11 @@
+import Typed from 'typed.js'
+
 // typing text animation script
-var typed = new Typed(".typing", {
+var type = new Typed(".typing", {
     strings: ["CEO of SAE","Engineer", "Developer", "Designer", "Freelancer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
 });
+
+console.log('ghr')
